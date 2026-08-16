29bd9f530caa050f494791da9912b59724296f74
Don-Claudio <claudiodex009@gmail.com>
2026-08-16 23:55:21 +0300

    feat: add apb_if, clocking block for driver-side timing, raw signal access for reset/monitor

---
+++ tb/apb_if.sv
@@ -0,0 +1,33 @@
+interface apb_if #(
+   parameter int DW = 32,
+   parameter int AW = 5
+) (
+   input logic pclk,
+   input logic presetn
+);
+
+   logic [AW-1:0] paddr;
+   logic          pwrite;
+   logic          psel;
+   logic          penable;
+   logic [DW-1:0] pwdata;
+   logic [DW/8-1:0] pstrb;
+   logic [DW-1:0] prdata;
+   logic          pready;
+   logic          pslverr;
+
+
+   clocking cb @(posedge pclk);
+    output paddr;
+    output pwrite, psel, penable;
+    output pwdata;
+    output pstrb;
+    input prdata;
+    input pready;
+    input pslverr;
+   endclocking : cb
+
+   modport TB (clocking cb);
+
+endinterface : apb_if
+
