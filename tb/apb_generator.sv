bd294e03a5b2cf2618a2bc8207db570d14f6714d
Don-Claudio <claudiodex009@gmail.com>
2026-08-19 21:19:01 +0300

    feat: add apb_generator, randomizes transactions and hands off via mailbox+event

---
+++ tb/apb_generator.sv
@@ -0,0 +1,28 @@
+class apb_generator #(parameter int DW = 32, parameter int AW = 5);
+
+   mailbox #(apb_transaction#(DW, AW)) gen2drv;
+   event                               drv_done;
+
+   function new(mailbox #(apb_transaction#(DW,AW)) mbx,
+            event done);
+
+    this.gen2drv = mbx;
+    this.drv_done = done;
+
+    endfunction
+
+   task run(int num_transactions);
+      apb_transaction#(DW, AW) tr;
+      repeat (num_transactions) begin
+        tr = new();
+
+        assert(tr.randomize()) else $fatal("%s:%0d Randomization failed", `__FILE__, `__LINE__);
+
+        gen2drv.put(tr);
+
+        @drv_done;
+
+      end
+   endtask
+
+endclass : apb_generator
