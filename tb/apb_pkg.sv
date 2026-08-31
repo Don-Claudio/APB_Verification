a69cd1902acaaa668752fecf2cc069a79095f165
Don-Claudio <claudiodex009@gmail.com>
2026-08-31 23:27:17 +0300

    feat: add apb_test, constructs env and runs 200 random transactions with drain before finish

--- tb/apb_pkg.sv
+++ tb/apb_pkg.sv
@@ -6,4 +6,6 @@ package apb_pkg;
     `include "apb_driver.sv"
     `include "apb_mon_txn.sv"
     `include "apb_scoreboard.sv"
+    `include "apb_env.sv"
+    `include "apb_test.sv"
 endpackage : apb_pkg
---
+++ tb/apb_test.sv
@@ -0,0 +1,21 @@
+class apb_test #(parameter int DW = 32, parameter int AW = 5);
+
+   apb_env#(DW, AW) env;
+   virtual apb_if#(DW,AW) vif;
+
+   function new(virtual apb_if#(DW, AW) vif);
+    this.vif = vif;
+
+   endfunction
+
+   task run();
+      env = new(vif);
+      env.run(200);
+
+      repeat (10) @(vif.cb);
+
+      $finish;
+
+   endtask
+
+endclass : apb_test
