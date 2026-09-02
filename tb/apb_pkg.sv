96b39441dcc5f6cab18bd372dbdca8ba0681aac9
Don-Claudio <claudiodex009@gmail.com>
2026-09-02 21:19:00 +0300

    fix: add missing apb_monitor include to apb_pkg, was causing unresolved type error in apb_env

--- tb/apb_pkg.sv
+++ tb/apb_pkg.sv
@@ -5,6 +5,7 @@ package apb_pkg;
     `include "apb_generator.sv"
     `include "apb_driver.sv"
     `include "apb_mon_txn.sv"
+    `include "apb_monitor.sv"
     `include "apb_scoreboard.sv"
     `include "apb_env.sv"
     `include "apb_test.sv"
